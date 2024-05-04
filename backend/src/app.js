707aebd88e754419d9450b2ac97c4466b14efeac
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-05-04 11:55:48 +0530

    added

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", userRoutes);
