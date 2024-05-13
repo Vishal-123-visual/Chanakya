b8a21dd08b8ea1d7041b1fd167d6dff3b5d23ff0
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-05-13 11:26:03 +0530

    added changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,9 +12,23 @@ import companyRoutes from "./routes/company.routes.js";
 import { BACKEND_URL, FRONTEND_URL } from "./config/config.js";
 
 const app = express();
-// app.use(cors());
 
-app.use(cors());
+// Define allowed origins
+const allowedOrigins = [FRONTEND_URL, BACKEND_URL];
+
+// CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// Apply CORS middleware with options
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -29,9 +43,5 @@ app.use("/api/paymentOptions", paymentOptionsRoutes);
 app.use("/api/company", companyRoutes);
 const __dirname = path.resolve();
 app.use("/api/images", express.static(path.join(__dirname + "/images")));
-//app.use(express.static(path.join(__dirname, "./build")));
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/build/index.html"));
-// });
 
 export default app;
